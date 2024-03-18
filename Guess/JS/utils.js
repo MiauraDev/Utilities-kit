213acffaa6fd1a8ab7d7f4514b4975b8bcb1de49
Laura Luque <lauraluque1215@gmail.com>
2024-03-18 13:01:13 -0500

    doc

--- Guess/JS/utils.js
+++
@@ -1,7 +0,0 @@
-export function getNumberRandom(start, end) {
-  return Math.floor(Math.random() * (end - start) + start);
-}
-
-export function getMessageAttempt(attemp) {
-  return `Tienes ${attemp} intentos`
-}
