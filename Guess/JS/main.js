24b42de9266e15527848981ed833f3e480ff9f5e
Laura Luque <lauraluque1215@gmail.com>
2024-03-18 00:23:52 -0500

    game

--- Guess/JS/main.js
+++ Guess/JS/main.js
@@ -1,10 +1,11 @@
 import { getMessageAttempt, getNumberRandom } from "./utils.js"
+import { changeTheme } from "./themes.js"
 
 const d = document
 
-const START_NUMBER = 100
+const START_NUMBER = 1
 const END_NUMBER = 150
-const NUMBER_ATTEMPS = 2
+const NUMBER_ATTEMPS = 5
 
 // seleccionando elementos del dom
 const input = d.querySelector('input')
@@ -13,6 +14,16 @@ const message = d.getElementById('message')
 const wrappedAttempts = d.getElementById('wrappedAttemps')
 const buttonReset = d.getElementById('reset')
 const attempMessage = d.getElementById('attempMessage')
+const startSpan1 = document.getElementById('start1');
+const endSpan1 = document.getElementById('end1');
+const startSpan2 = document.getElementById('start2');
+const endSpan2 = document.getElementById('end2');
+
+// Asignamos el rango de edad
+startSpan1.textContent = START_NUMBER;
+endSpan1.textContent = END_NUMBER;
+startSpan2.textContent = START_NUMBER;
+endSpan2.textContent = END_NUMBER;
 
 // añadiendo eventos
 submit.addEventListener('click', handleClickSend)
@@ -23,6 +34,7 @@ let attempts = []
 let mysteriousNumber = getNumberRandom(START_NUMBER, END_NUMBER)
 console.log(mysteriousNumber)
 attempMessage.textContent = getMessageAttempt(NUMBER_ATTEMPS)
+
 
 function handleClickSend() {
   if (!(validateNumber())) {
@@ -35,16 +47,18 @@ function handleClickSend() {
   if (currentAttempt === NUMBER_ATTEMPS) {
     message.textContent = 'Ya has superado el limite de intentos, has perdido!'
     input.disabled = true
+    changeTheme('full');
     return
   }
 
   if (inputValue === mysteriousNumber) {
+    changeTheme('fun');
     message.textContent = 'Felicidades adivinaste mi edad'
     input.disabled = true
   } else if (inputValue > mysteriousNumber) {
-    message.textContent = 'El número es muy alto!'
+    message.textContent = 'NO SOY TAN MAYOR!'
   } else if (inputValue < mysteriousNumber) {
-    message.textContent = 'El número es muy bajo!'
+    message.textContent = 'NO SOY TAN MENOR!'
   }
 
   attempts.push(inputValue)
@@ -52,6 +66,11 @@ function handleClickSend() {
   input.value = ''
   ++currentAttempt
   attempMessage.textContent = `Te quedan ${NUMBER_ATTEMPS - currentAttempt} intentos!`
+    
+    // Verificar si el número de intentos llegó a cero
+    if (currentAttempt >= NUMBER_ATTEMPS) {
+      changeTheme('full'); // Llamada a la función del otro archivo para cambiar el tema
+    }
 }
 
 function paintAttemps(attempts) {
@@ -74,6 +93,8 @@ function resetGame() {
   attempMessage.textContent = `Tienes ${NUMBER_ATTEMPS - currentAttempt} intentos!`
   message.textContent = ''
   input.disabled = false
+  mysteriousNumber= getNumberRandom(START_NUMBER,END_NUMBER); 
+  console.log( mysteriousNumber);  
 }
 
 function validateNumber() {
